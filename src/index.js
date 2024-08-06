84c34b9c798bf61c6fc661ea517d3bc933433f2a
Kris PATIL <kris.patil@energyaustralia.com.au>
2024-08-06 12:15:08 +1000

    v1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const app = require('express')();
+
+app.get('/', (req, res ) => res.json({ message: 'Hello from Docker' }););
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
